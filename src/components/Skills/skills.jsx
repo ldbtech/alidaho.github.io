56b5a00f2e356980e9be180c1fd646b97e90a69a
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2023-10-03 06:08:56 -0400

    Fetch data for skills to show on the frontend as well

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './skills.css';
+import { database } from "../../Firebase";
+import { ref, onValue } from "firebase/database";
+import { TailSpin } from "react-loader-spinner";
+
 const Skills = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const postRef = ref(database, 'website/Skills');
+
+            try {
+                const snapshot = await onValue(postRef, (snapshot) => {
+                    const dataVal = snapshot.val();
+                    if (dataVal) {
+                        const dataArray = Object.keys(dataVal).map((key) => ({
+                            id: key,
+                            ...dataVal[key],
+                        }));
+                        setData(dataArray);
+                    } else {
+                        setData([]);
+                    }
+                    setLoading(false); // Set loading to false after fetching data
+
+                });
+
+                // Unsubscribe from the listener to prevent multiple executions
+                return () => {
+                    snapshot();
+                };
+            } catch (error) {
+                console.error(`Error Fetching data ${error}`);
+                setLoading(false); // Set loading to false in case of an error
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependencies array to run once
+    console.log("Data Skills", data);
     return (
         <section id="skills">
             <span className="skillTitle">What I do</span>
             <span className="skillDesc">I am skilled Software and AI engineer</span>
             <div className="skillBars">
-                <div className="skillBar">
-                    <img src="" alt="Web & App DEV" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Web & App UI/UX | DEV</h2>
-                        <p>This is a demo text</p>
+                {data.map((skillful) => (
+                    <div className="skillBar">
+                        <div className="skillBarImgContainer">
+                            <img src={skillful.Picture} alt="Web & App DEV" className="skillBarImg" />
+                        </div>
+                        <div className="skillBarContent">
+                            <div className="skillBarText">
+                                <h2>{skillful.Title}</h2>
+                                <p>{skillful.Description}</p>
+                            </div>
+                            <div className="skillTags">
+                                {skillful.Tags.split(',').map((tag, index) => (
+                                    <span key={index} className="skillTag">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+
+                        </div>
+
                     </div>
-                </div>
-                <div className="skillBar">
-                    <img src="" alt="Artificial Intelligence" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Artificial Intelligence </h2>
-                        <p>This is a demo text</p>
-                    </div>
-                </div>
-                <div className="skillBar">
-                    <img src="" alt="Data Engineering" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Data Engineering</h2>
-                        <p>This is a demo text</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
