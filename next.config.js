56febe3cff3a8937e104f04af03c8c1a0c549ff5
alidaho <ari.dahoa@gmail.com>
2025-05-31 00:40:35 -0400

    added thoughts

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/alidaho.github.io' : '',
     trailingSlash: true,
     assetPrefix: process.env.NODE_ENV === 'production' ? '/alidaho.github.io/' : '',
+    onDemandEntries: {
+        // period (in ms) where the server will keep pages in the buffer
+        maxInactiveAge: 25 * 1000,
+        // number of pages that should be kept simultaneously without being disposed
+        pagesBufferLength: 2,
+    },
 };
 
 module.exports = nextConfig;
