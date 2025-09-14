a0a9efa4721b1d382038353073878b90de17ceb0
alidaho <ari.dahoa@gmail.com>
2025-09-14 19:43:38 -0400

    try something different to deploy to vercel

--- next.config.js
+++ next.config.js
@@ -14,12 +14,6 @@ const nextConfig = {
     },
     // Remove basePath and assetPrefix for Vercel deployment
     // These are only needed for GitHub Pages
-    onDemandEntries: {
-        // period (in ms) where the server will keep pages in the buffer
-        maxInactiveAge: 25 * 1000,
-        // number of pages that should be kept simultaneously without being disposed
-        pagesBufferLength: 2,
-    },
 };
 
 module.exports = nextConfig;
